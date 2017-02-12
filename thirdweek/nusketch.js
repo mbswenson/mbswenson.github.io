459859d6b4f696da6c6d66d313eb6a9da903f7fa
Macy Swenson <swenm258@newschool.edu>
2017-02-12 13:25:15 -0500

    test box

--- thirdweek/nusketch.js
+++ thirdweek/nusketch.js
@@ -1,13 +1,13 @@
 var canvas;
+var sliderslide;
+var sliderslideslide;
 
 function setup(){
-	canvas = createCanvas(500, 500);
+	canvas = createCanvas(500, 500, WEBGL);
 	canvas.position(300, 50);
-	canvas.class("lemon");
 }
 
 function draw(){
-	background (220, 180, 200);
-	ellipse (width/2, height/2, 100, 100);
-	ellipse (width/2, height/2, 50, 50)
+	background (0);
+	box (200, 200, 200);
 }
