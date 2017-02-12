bcda60cf02493ea0e15b9ca894ac421f9b568e0a
Macy Swenson <swenm258@newschool.edu>
2017-02-12 12:28:42 -0500

    ya did some new things

---
+++ thirdweek/nusketch.js
@@ -0,0 +1,13 @@
+var canvas;
+
+function setup(){
+	canvas = createCanvas(600, 400);
+	canvas.position(300, 50);
+	canvas.class("lemon");
+}
+
+function draw(){
+	background (220, 180, 200);
+	ellipse (width/2, height/2, 100, 100);
+	ellipse (width/2, height/2, 50, 50)
+}
