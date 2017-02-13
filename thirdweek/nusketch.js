ef2456ec4e1953fbc46cbf217e10a2509f1422ab
Macy Swenson <swenm258@newschool.edu>
2017-02-12 20:24:53 -0500

    oohboy

--- thirdweek/nusketch.js
+++ thirdweek/nusketch.js
@@ -17,9 +17,11 @@ function setup(){
 }
 
 function draw(){
-	fill(colorslide.value(), 255, 255);
+	fill(colorslide.value(), 255, 100);
 	strokeWeight(strokeslide.value());
 	ellipse(width/4, height/2, sizeslide.value(), sizeslide.value());
+	fill(colorslide.value(), 255, 150);
 	ellipse(width/2, height/2, sizeslide.value(), sizeslide.value());
+	fill(colorslide.value(), 255, 200);
 	ellipse(width/4+width/2, height/2, sizeslide.value(), sizeslide.value());
 }
