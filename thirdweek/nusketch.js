828ba483ef5cd0fa1e2cbe665e4d5655ffeeb04b
Macy Swenson <swenm258@newschool.edu>
2017-02-12 21:42:50 -0500

    beyonce has driven me through this evening

--- thirdweek/nusketch.js
+++ thirdweek/nusketch.js
@@ -9,11 +9,11 @@ function setup(){
 	background (0);
 	colorMode(HSB, 255);
 	colorslide = createSlider (0, 255, 0);
-	colorslide.position (600, 610);
+	colorslide.position (680, 610);
 	strokeslide = createSlider (1, 20, 1);
-	strokeslide.position (600, 630);
+	strokeslide.position (680, 630);
 	sizeslide = createSlider (100, 400, 100);
-	sizeslide.position (600, 650);
+	sizeslide.position (680, 650);
 }
 
 function draw(){
