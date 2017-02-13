a49625423f52d9009b443aaaa94a16ae919fbd77
Macy Swenson <swenm258@newschool.edu>
2017-02-12 19:08:11 -0500

    fixed the sliders again ugh

--- thirdweek/nusketch.js
+++ thirdweek/nusketch.js
@@ -7,12 +7,13 @@ function setup(){
 	canvas = createCanvas(600, 600);
 	canvas.position(300, 50);
 	background (0);
-	colorslide = createSlider (0, 255, 0)
-	colorslide.position (width/2, 500)
-	strokeslide = createSlider (1, 20, 1)
-	strokeslide.position (width/2, 520)
-	sizeslide = createSlider (100, 400, 100)
-	strokeslide.position (width/2, 550)
+	colorMode(HSB, 255);
+	colorslide = createSlider (0, 255, 0);
+	colorslide.position (width/2, 500);
+	strokeslide = createSlider (1, 20, 1);
+	strokeslide.position (width/2, 520);
+	sizeslide = createSlider (100, 400, 100);
+	strokeslide.position (width/2, 550);
 }
 
 function draw(){
