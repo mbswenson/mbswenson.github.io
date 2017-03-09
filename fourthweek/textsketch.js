fda2b3e9b805d54e8a67d382a423eb3b99d8906f
Macy Swenson <swenm258@newschool.edu>
2017-03-09 15:41:59 -0500

    eh

--- fourthweek/textsketch.js
+++ fourthweek/textsketch.js
@@ -6,9 +6,9 @@ function setup(){
 	noCanvas();
 	textLocale = createElement('textArea', 'enter text');
 	textLocale.size(500,500);
+	textLocale.color(#4B0082);
 	createElement('br');
 	buttbutt = createButton ('push to save');
-	buttbutt.color(#4B0082);
 	buttbutt.mousePressed(saveText);
 }
 
