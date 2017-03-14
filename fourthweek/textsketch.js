281e0377bd784a8fb4ebf20175b40fee4d9b120b
Macy Swenson <swenm258@newschool.edu>
2017-03-14 14:44:24 -0400

    test test test

--- fourthweek/textsketch.js
+++ fourthweek/textsketch.js
@@ -7,37 +7,36 @@ function setup(){
 	textLocale = createElement('textArea', 'write how you feel');
 	textLocale.size(200,200);
 	createElement('br');
-	buttbutt = createButton ('push to save');
+	buttbutt = createButton ('dissociate from how you feel');
 	buttbutt.mousePressed(saveText);
 }
 
 function saveText(){
 	rememberedText = textLocale.value();
-	createP("ORIGINAL TEXT: " + rememberedText);
 	var textArr = splitTokens(rememberedText, '.:;?! @#$%^&*()-+');
 	var length = textArr.length;
 
 	for (var i = 0; i < textArr.length; i++){
 		if (textArr[i] === "I" || textArr[i] === "i"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}
 		if (textArr[i] === "I'm" || textArr[i] === "i'm"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}
 		if (textArr[i] === "I've" || textArr[i] === "i've"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}	
 		if (textArr[i] === "My" || textArr[i] === "my"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}
 		if (textArr[i] === "FEELING" || textArr[i] === "feeling"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}
 		if (textArr[i] === "FEEL" || textArr[i] === "feel"){
-			textArr[i] = "";
+			textArr[i] = " ";
 		}
 
 	}
 	var newString = join(textArr, '');
-	createP("NEW TEXT: " + newString);
+	createP(newString);
 }
