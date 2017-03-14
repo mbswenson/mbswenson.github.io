4730be7e8602e1f224c44304daaf221343c1af68
Macy Swenson <swenm258@newschool.edu>
2017-03-14 14:26:51 -0400

    test test test

--- fourthweek/textsketch.js
+++ fourthweek/textsketch.js
@@ -4,7 +4,7 @@ var rememberedText;
 
 function setup(){
 	noCanvas();
-	textLocale = createElement('textArea', 'enter text');
+	textLocale = createElement('textArea', 'write how you feel');
 	textLocale.size(200,200);
 	createElement('br');
 	buttbutt = createButton ('push to save');
@@ -16,6 +16,28 @@ function saveText(){
 	createP("ORIGINAL TEXT: " + rememberedText);
 	var textArr = splitTokens(rememberedText, '.:;?! @#$%^&*()-+');
 	var length = textArr.length;
+
+	for (var i = 0; i < textArr.length; i++){
+		if (textArr[i] === "I" || textArr[i] === "i"){
+			textArr[i] = "";
+		}
+		if (textArr[i] === "I'm" || textArr[i] === "i'm"){
+			textArr[i] = "";
+		}
+		if (textArr[i] === "I've" || textArr[i] === "i've"){
+			textArr[i] = "";
+		}	
+		if (textArr[i] === "My" || textArr[i] === "my"){
+			textArr[i] = "";
+		}
+		if (textArr[i] === "FEELING" || textArr[i] === "feeling"){
+			textArr[i] = "";
+		}
+		if (textArr[i] === "FEEL" || textArr[i] === "feel"){
+			textArr[i] = "";
+		}
+
+	}
 	var newString = join(textArr, '');
 	createP("NEW TEXT: " + newString);
 }
