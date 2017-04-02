33fe3c26bacafb1e635d414cbfb5e4c34c347c7f
Macy Swenson <swenm258@newschool.edu>
2017-04-02 16:21:57 -0400

    added a whole buncha stuff

--- eighthweek/concord.js
+++ eighthweek/concord.js
@@ -1,6 +1,7 @@
 var buttbutt;
 var textLocale;
 var rememberedText;
+var concordance = {};
 
 function setup(){
 	noCanvas();
@@ -16,4 +17,34 @@ function saveText(){
 	rememberedText = textLocale.value();
 	var textArr = splitTokens(rememberedText, '.:;?! @#$%^*()-+=[]{}/')
     var length = textArr.length;
+}
+
+function concordanceMaker(){
+	data = saveText.value();
+	var tokens = data.split(/\W+/);
+
+	var keys = [];
+	for (var i = 0; i < tokens.length; i++){
+		var word = tokens[i].toLowerCase();
+		if (concordance[word] === undefined){
+			concordance[word] = 1;
+			keys.push(word);
+	  } else {
+	  	concordance[word]++;
+	  }
+		}
+
+	keys.sort(function(a, b){
+		return (concordance[b] - concordance[a]);
+	});
+
+	for (var i = 0; i < keys.length; i++) {
+		console.log(keys[i] + ': ' + concordance[keys[i]]);
+	}
+
+	var ul = select('#concordance');
+	for (var i = 0; i < keys.length; i++) {
+		var li = createElement ('li', keys[i] + ': ' + concordance[keys[i]]);
+		li.parent(ul);
+	}
 }
