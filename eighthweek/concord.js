d59483c9369e516f3c4f9bc7d36fd1d3e158a33e
Macy Swenson <swenm258@newschool.edu>
2017-04-02 14:05:29 -0400

    drop that phone

---
+++ eighthweek/concord.js
@@ -0,0 +1,17 @@
+var buttbutt;
+var textLocale;
+var rememberedText;
+
+function setup(){
+	canvas = createCanvas (650, 650);
+	textLocale.size(500, 500);
+	createElement('br');
+	createElement('br');
+	buttbutt = createButton ('boop');
+	buttbutt.mousePressed(saveText);
+
+function saveText(){
+	rememberedText = textLocale.value();
+	var textArr = splitTokens(rememberedText, '.:;?! @#$%^*()-+=[]{}/')
+    var length = textArr.length;
+}
