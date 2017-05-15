0c2dd1044eef703f55e9cfe5b8cbcdca30022b2d
Macy Swenson <swenm258@newschool.edu>
2017-05-15 05:14:08 -0400

    oof

--- eighthweek/concord.js
+++ eighthweek/concord.js
@@ -58,6 +58,6 @@ function draw(){
   textSize(10);
   stroke(0);
   for (var i = 0; i < keys.length; i++){
-  text(keys[i]*, concordance[keys[i]]*60, concordance[keys[i]]*60);
+    text(keys[i], concordance[keys[i]]*60, concordance[keys[i]]*60);
 }
 }
