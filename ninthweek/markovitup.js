b979f88ae56e6106fab8e9ed972c7ac127fd6431
Macy Swenson <swenm258@newschool.edu>
2017-05-15 15:28:04 -0400

    ahahaha

--- ninthweek/markovitup.js
+++ ninthweek/markovitup.js
@@ -8,7 +8,10 @@ function preload() {
 
 function setup() {
 
-  createCanvas(500, 500);
+   var cnv = createCanvas(500, 500);
+  var x = (windowWidth - width) / 2;
+  var y = (windowHeight - height) / 2;
+  cnv.position(x, y);
   textFont('times', 16);
   textAlign(LEFT);
 
